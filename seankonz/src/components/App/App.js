e4c569b0b4cae86780fe54644033921d980b3f18
Sean <swkonz@gmail.com>
2019-07-14 21:03:18 -0700

    small fix

--- seankonz/src/components/App/App.js
+++ seankonz/src/components/App/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
+      <h2>Sean's website is under construction!</h2>
     </div>
   );
 }
