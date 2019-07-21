60aa6fed464af222846b99a032fde85dfdcf9855
Sean <swkonz@gmail.com>
2019-07-20 17:23:39 -0700

    new app, no bootstrap

--- sean-portfolio/src/index.js
+++ sean-portfolio/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './custom.scss';
 import './index.css';
-import App from './components/App/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
